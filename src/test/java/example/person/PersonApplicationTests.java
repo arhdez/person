52061822e4c975a91f798e9ac9851e795227a331
yurien.lopez <yurien.lopez@digitalturbine.com>
2024-10-22 13:37:50 -0500

    Disabling security for testing.

--- src/test/java/example/person/PersonApplicationTests.java
+++ src/test/java/example/person/PersonApplicationTests.java
@@ -6,25 +6,33 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestSecurityConfig.class)  // Import the test security configuration to bypass security auth
 class PersonApplicationTests {
 
-	@Autowired
-	TestRestTemplate restTemplate;
+    @Autowired
+    TestRestTemplate restTemplate;
 
-	@Test
-	void shouldReturnACashCardWhenDataIsSaved() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/persons/99", String.class);
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    @Test
+    void shouldReturnACashCardWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/persons/99", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
-		DocumentContext documentContext = JsonPath.parse(response.getBody());
-		Number id = documentContext.read("$.id");
-		assertThat(id).isEqualTo(99);
-	}
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isEqualTo(99);
+    }
+
+    @Test
+    void shouldGetAllPersons() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/persons", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getHeaders().getContentType().toString()).isEqualTo("application/json");
+    }
 }
