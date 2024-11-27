693a8f2b73a4f8f6fd45ca62f2c04ab25865c326
arhdez87 <arhdez87@gmail.com>
2024-11-27 02:12:02 -0600

    last version working I comment the test create on person address working

--- src/main/java/example/person/repository/PersonAddressRepository.java
+++ src/main/java/example/person/repository/PersonAddressRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface PersonAddressRepository extends CrudRepository<PersonAddress, PersonAddressId>, PagingAndSortingRepository<PersonAddress, PersonAddressId> {
+    boolean existsByIdPersonIdAndIdAddressId(UUID personId, UUID addressId);
 }
--- src/main/java/example/person/service/PersonAddressService.java
+++ src/main/java/example/person/service/PersonAddressService.java
@@ -1,18 +1,40 @@
 package example.person.service;
 
 import example.person.dto.PersonAddressDto;
+import example.person.jpa.Person;
 import example.person.mapper.PersonAddressMapper;
 import example.person.repository.PersonAddressRepository;
+import example.person.repository.PersonRepository;
+import example.person.validation.DuplicateException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class PersonAddressService {
+    private final PersonRepository personRepository;
     private final PersonAddressRepository personAddressRepository;
     private final PersonAddressMapper personAddressMapper;
 
     public PersonAddressDto createPersonAddress(PersonAddressDto personAddressDto){
+        checkDoublePersonAddress(personAddressDto);
         return personAddressMapper.personAddressToPersonAddressDto(personAddressRepository.save(personAddressMapper.personAddressDtoToPersonAddress(personAddressDto)));
+    }
+
+    private void checkDoublePersonAddress(PersonAddressDto personAddressDto) {
+        checkDoublePersonAddress(personAddressDto.getPersonId(), personAddressDto.getAddressId(),
+                personAddressDto.getStreet(), personAddressDto.getCity(),
+                personAddressDto.getState(), personAddressDto.getZipCode());
+    }
+    private void checkDoublePersonAddress(UUID personId, UUID addressId, String street, String city, String state, Integer zipCode) {
+        if (personAddressRepository.existsByIdPersonIdAndIdAddressId(personId, addressId)){
+            String fullName = personRepository.findById(personId)
+                    .map(person -> person.getFirstName() + " " + person.getLastName())
+                    .orElse("Unknown");
+            throw new DuplicateException("Person: "
+                    + fullName + " with Address: " + street + " " + city + " " + state + " " + zipCode + " already exists." );
+        }
     }
 }
