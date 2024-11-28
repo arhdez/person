391dc297c5ca1f02f6e8aa4c802208094d48d786
arhdez87 <arhdez87@gmail.com>
2024-11-28 09:40:58 -0600

    last version working I comment the test POST and GET on person address working

--- src/main/java/example/person/service/PersonAddressService.java
+++ src/main/java/example/person/service/PersonAddressService.java
@@ -3,9 +3,7 @@ package example.person.service;
 import example.person.dto.PersonAddressDto;
 import example.person.dto.PersonDto;
 import example.person.dto.PersonWithPersonAddressDto;
-import example.person.jpa.Person;
 import example.person.mapper.PersonAddressMapper;
-import example.person.mapper.PersonMapper;
 import example.person.mapper.PersonWithPersonAddressMapper;
 import example.person.repository.PersonAddressRepository;
 import example.person.repository.PersonRepository;
@@ -27,20 +25,15 @@ import java.util.stream.Collectors;
 public class PersonAddressService {
     private final PersonRepository personRepository;
     private final PersonAddressRepository personAddressRepository;
-    private final PersonMapper personMapper;
     private final PersonAddressMapper personAddressMapper;
     private final PersonWithPersonAddressMapper personWithPersonAddressMapper;
     private final PersonService personService;
 
     public PersonWithPersonAddressDto createPersonAddress(PersonAddressDto personAddressDto){
         checkDoublePersonAddress(personAddressDto);
-        Optional<PersonDto> personDto = personService.findById(personAddressDto.getPersonId());
-        if(personDto.isEmpty()) {
-            throw new DoesNotExistsException("The Person does not exist");
-        }
+        PersonDto personDto = getValidPerson(personAddressDto);
         PersonAddressDto createdPersonAddressDto = personAddressMapper.personAddressToPersonAddressDto(personAddressRepository.save(personAddressMapper.personAddressDtoToPersonAddress(personAddressDto)));
-            return personWithPersonAddressMapper.toPersonWithPersonAddressDto(personDto.get(), createdPersonAddressDto);
-
+            return personWithPersonAddressMapper.toPersonWithPersonAddressDto(personDto, createdPersonAddressDto);
         //return personAddressMapper.personAddressToPersonAddressDto(personAddressRepository.save(personAddressMapper.personAddressDtoToPersonAddress(personAddressDto)));
     }
 
@@ -54,6 +47,11 @@ public class PersonAddressService {
                     return personWithPersonAddressMapper.toPersonWithPersonAddressDto(personDto, personAddressDto);
                 }).collect(Collectors.toList());
     }
+    /*public List<PersonAddressDto> findAllPersonWithPersonAddress(Pageable pageable){
+        PageRequest pageRequest =
+                PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), pageable.getSortOr(Sort.by(Sort.Direction.ASC, "firstName")));
+        return personAddressRepository.findAll(pageRequest).stream().map(personAddressMapper::personAddressToPersonAddressDto).collect(Collectors.toList())
+    }*/
 
     private PersonDto getValidPerson(PersonAddressDto personAddress) {
         Optional<PersonDto> personDto = personService.findById(personAddress.getPersonId());
@@ -62,12 +60,6 @@ public class PersonAddressService {
         }
         return personDto.get();
     }
-
-    /*public List<PersonAddressDto> findAllPersonWithPersonAddress(Pageable pageable){
-        PageRequest pageRequest =
-                PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), pageable.getSortOr(Sort.by(Sort.Direction.ASC, "firstName")));
-        return personAddressRepository.findAll(pageRequest).stream().map(personAddressMapper::personAddressToPersonAddressDto).collect(Collectors.toList())
-    }*/
 
     private void checkDoublePersonAddress(PersonAddressDto personAddressDto) {
         checkDoublePersonAddress(personAddressDto.getPersonId(), personAddressDto.getAddressId(),
