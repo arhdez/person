0fbeac931c3dce1c61a9e88bbebe3e99d82492c0
arhdez87 <arhdez87@gmail.com>
2025-01-14 08:31:45 -0600

    Working with kafka producing the right message

---
+++ src/main/java/example/person/config/KafkaProducerConfig.java
@@ -0,0 +1,41 @@
+package example.person.config;
+
+import example.person.dto.PersonAddressKafkaDto;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
+
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
+
+    @Bean
+    public ProducerFactory<String, PersonAddressKafkaDto> producerFactory() {
+        Map<String, Object> producerProps = new HashMap<>();
+        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+
+        return new DefaultKafkaProducerFactory<>(producerProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, PersonAddressKafkaDto> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+}
